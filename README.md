83dd5f3018dab83eaec434d46c296d881bc1923e
Alex Gusev <alex@flancer64.com>
2016-09-16 17:10:55 +0300

    Initialization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sample_mage2_admin_grid
-Magento 2 sample: Add grid to Admin UI
+
+Magento 2 sample: Adding grid to Admin UI.
---
+++ src/Config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Container for module's constants (hardcoded configuration).
+ *
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Flancer32\Sample;
+
+class Config {
+    const MODULE = 'Flancer32_Sample';
+}
