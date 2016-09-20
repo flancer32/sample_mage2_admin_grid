2c170ab8a0eec2f5ae521591c2b0bacf4a0f473e
Alex Gusev <flancer64@gmail.com>
2016-09-20 07:08:10 +0300

    remove extra code from controller

--- src/Controller/Adminhtml/Grid/Index.php
+++ src/Controller/Adminhtml/Grid/Index.php
@@ -2,46 +2,35 @@
 /**
  * User: Alex Gusev <alex@flancer64.com>
  */
-    namespace Flancer32\Sample\Controller\Adminhtml\Grid;
+namespace Flancer32\Sample\Controller\Adminhtml\Grid;
+
+/**
+ * Controller for "/admin/sample/index".
+ */
+class Index
+    extends \Magento\Backend\App\Action
+{
+    const ACL_RESOURCE = 'Flancer32_Sample::sample_grid';
+    const MENU_ITEM = 'Flancer32_Sample::sample_grid';
+    const TITLE = 'Sample Grid';
 
     /**
-     * Controller for "/admin/sample/index".
+     * Check ACL permissions.
+     *
+     * @return bool
      */
-    class Index
-        extends \Magento\Backend\App\Action
+    protected function _isAllowed()
     {
-        const ACL_RESOURCE = 'Flancer32_Sample::sample_grid';
-        const MENU_ITEM = 'Flancer32_Sample::sample_grid';
-        const TITLE = 'Sample Grid';
-
-        /** @var \Magento\Framework\View\Result\PageFactory */
-        protected $_resultPageFactory;
-
-        public function __construct(
-            \Magento\Backend\App\Action\Context $context,
-            \Magento\Framework\View\Result\PageFactory $resultPageFactory
-        ) {
-            parent::__construct($context);
-            $this->_resultPageFactory = $resultPageFactory;
-        }
-
-        /**
-         * Check ACL permissions.
-         *
-         * @return bool
-         */
-        protected function _isAllowed()
-        {
-            $result = $this->_authorization->isAllowed(self::ACL_RESOURCE);
-            return $result;
-        }
-
-        public function execute()
-        {
-            /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
-            $resultPage = $this->_resultPageFactory->create();
-            $resultPage->setActiveMenu(self::MENU_ITEM);
-            $resultPage->getConfig()->getTitle()->prepend(__(self::TITLE));
-            return $resultPage;
-        }
+        $result = $this->_authorization->isAllowed(self::ACL_RESOURCE);
+        return $result;
     }
+
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_PAGE);
+        $resultPage->setActiveMenu(self::MENU_ITEM);
+        $resultPage->getConfig()->getTitle()->prepend(__(self::TITLE));
+        return $resultPage;
+    }
+}
